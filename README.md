f70bfdaedef85d4f3bf63c499fa715d65b45b3c8
mattmarnien <mgm215@gmail.com>
2020-03-15 20:32:17 -0400

    added show info on click

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # watchTheShow
 
-Watch the show is a simple app to track the shows you want to watch, track what youve seen, and remind yourself to watch some again. The application is fully responsive across mobile device sizes/
+Watch the show is a simple app to track the shows you want to watch, track what youve seen, and remind yourself to watch some again. The application is fully responsive across mobile device sizes. Additionally, clicking the show title on either page will give a breif snippet and image from OMDB about the show (assuming the title is entered properly).
 
 http://watchtheshow.herokuapp.com/
 
@@ -12,7 +12,7 @@ Watch the show was made using html, css, bootstrap, javascript, jquery, mysql, n
 
 # Future Plans:
 
-I plan to add individual user accounts to track by user as well as add information about each show added and options for streaming the show.
+I plan to add individual user accounts to track by user as well as add information about each show added and options for streaming the show. Additionally, validation for show name would be helpful.
 
 # Images
 
--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,3 +1,6 @@
+const apiKey = "ce3fefc1";
+
+// changes a show from not watched to watched
 $(function () {
     $(".changeWatched").on("click", function (event) {
         let id = $(this).data("id");
@@ -14,11 +17,12 @@ $(function () {
 
 })
 
+// stops page from reloading on submit
 $("#addForm").on("submit", function(event){
     event.preventDefault();
 
 })
-
+// adds a new show on form submit
 $("#addButton").on("click", function(event){
     if($("#addInput").val().trim() !== ''){
     let newShow = {
@@ -31,6 +35,7 @@ $("#addButton").on("click", function(event){
 }
 })
 
+// moves a show from watched to the archives
 $(".archive").on("click", function(event){
         let id = $(this).data("id");
         $.ajax("api/shows/archive/" + id, {
@@ -41,7 +46,7 @@ $(".archive").on("click", function(event){
             }
     )
 })
-
+//brings a show back from the archive to the watch-list for a rewatch
 $(".backToWatchList").on("click", function(event){
     let id = $(this).data("id");
     $.ajax("api/shows/unArchive/" + id, {
@@ -52,3 +57,39 @@ $(".backToWatchList").on("click", function(event){
         }
 )
 })
+
+// clicking a show name queries OMDB and displays show info and removes previous show info
+$('.showDiv').on("click", function(event){
+    let target =$(this);
+    let name = $(this).data("name");
+    console.log(name);
+    $('.removable').remove();
+    queryURL = "http://www.omdbapi.com/?t=" + name + "&apikey=" + apiKey
+    $.ajax(queryURL, {
+        type: "GET",
+        url: queryURL
+    }).then(data => {
+        console.log(data, target);
+        genShowInfo(data, target);
+    
+
+
+    })
+})
+
+
+// compiles show info, puts it into elements and posts to the page
+function genShowInfo(data, target){
+    let newDiv= $("<div class='removable'>");
+    let title = $("<h1>");
+    title.text(data.Title);
+    let poster = $(`<img src='${data.Poster}' style='width:100px;height:200px;'>`)
+    let rating = $("<h5>");
+    rating.text(data.Rated);
+    let genre = $("<h5>");
+    genre.text(data.Genre);
+    let plot = $("<p>");
+    plot.text(data.Plot);
+    target.append(newDiv);
+    newDiv.append(title, poster, rating, genre, plot);
+}
